9886177a3f2f60bd8fffd34e3bb49fe5b9ffa60e
Giorgio Zoppi <sba23101@student.cct.ie>
2025-02-05 15:03:53 +0000

    modified:   brackets.py

--- stacks_queue/brackets.py
+++ stacks_queue/brackets.py
@@ -1,4 +1,20 @@
-from stack_array import Stack
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
 
 def equation_checker(equation):
     """
@@ -14,7 +30,7 @@ def equation_checker(equation):
         if char == "(":
             stack.push(char)
         elif char == ")":
-            if stack.pop() == None:
+            if stack.pop() is None:
                 return False
 
     if stack.size() == 0:
