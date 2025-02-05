c8cac245c2a4937fd2703badeabd1d7de2dc1095
Giorgio Zoppi <sba23101@student.cct.ie>
2025-02-05 15:01:28 +0000

    add parentesis

--- stacks_queue/brackets.py
+++ stacks_queue/brackets.py
@@ -9,15 +9,19 @@ def equation_checker(equation):
     Returns:
        bool: Return if parentheses are balanced or not
     """
-    s = Stack()
-    for c in equation:
-        if c == '(':
-            s.push(s)
-        elif c == ')':
-            data = s.pop()
-            if data is None:
+    stack = Stack()
+    for char in equation:
+        if char == "(":
+            stack.push(char)
+        elif char == ")":
+            if stack.pop() == None:
                 return False
-    if s.size() == 0:
+
+    if stack.size() == 0:
         return True
     else:
         return False
+if __name__=="__main__":
+    print ("Pass" if (equation_checker('((3^2 + 8)*(5/2))/(2+6)')) else "Fail")
+    print ("Pass" if not (equation_checker('((3^2 + 8)*(5/2))/(2+6))')) else "Fail")
+    
