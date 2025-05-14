f844cb760e746108b891418ff20e6be1676155e9
Giorgio Zoppi <sba23101@student.cct.ie>
2025-05-14 18:58:22 +0200

    modified:   find_duplicate_number.py

--- praticeproblems/tier1/fast_slow_pointer/find_duplicate_number.py
+++ praticeproblems/tier1/fast_slow_pointer/find_duplicate_number.py
@@ -1,1 +1,14 @@
-Given an array of positive numbers, nums, such that the values lie in the range [1,n][1,n], inclusive, and that there are n+1n+1 numbers in the array, find and return the duplicate number present in nums. There is only one repeated number in nums, but it may appear more than once in the array.
+from typing import List
+def findDuplicate(nums: List[int]) -> int:
+    slow,fast = nums[0], nums[0]
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    # find the netrance
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    return fast
